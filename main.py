cfdb39739b8cc2f389981a3d461146c913d3d19c
flyskywalkerlby <flyskywalkerlby@gmail.com>
2024-04-19 22:56:23 -0400

    mae evaluate

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
+from tqdm import tqdm
 
 from transformers import DetrForObjectDetection, DetrImageProcessor
 
@@ -43,8 +44,9 @@ def get_data_loaders(
     return train_loader, test_loader
 
 
-def evaluate(model, test_loader, criterion, device):
+def evaluate(model, test_loader, device):
     model.eval()
+    criterion = nn.L1Loss()
     total_loss = 0.0
     with torch.no_grad():
         for inputs, labels in test_loader:
@@ -63,7 +65,7 @@ def train_model(model, train_loader, test_loader, num_epochs=10):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.MSELoss()
 
-    for epoch in range(num_epochs):
+    for epoch in tqdm(range(num_epochs)):
         model.train()
         running_loss = 0.0
         for inputs, labels in train_loader:
@@ -77,7 +79,7 @@ def train_model(model, train_loader, test_loader, num_epochs=10):
             running_loss += loss.item()
 
         print(f"Epoch {epoch + 1}, Loss: {running_loss / len(train_loader)}")
-        evaluate(model, test_loader, criterion, device)
+        evaluate(model, test_loader, device)
 
 
 class AnglePredictor(nn.Module):
